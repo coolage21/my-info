0add843d1d7e3c7fddb7e7dc0894d3b3608522cd
DESKTOP-EQHITR0\forwiz014 <coolage512@gmail.com>
2026-04-24 13:41:12 +0900

    오류 코드 수정

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -1,22 +1,19 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
 import { projects } from './../data'
 
 export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> } 
 ) {
- const { id } = await params; // 여기서 await를 추가합니다.
 
-  const project = projects.find(
-    (item) => item.id === Number(id)
-  )
+  const { id } = await params;
+
+
+  const project = projects.find((item) => item.id === Number(id));
 
   if (!project) {
-    return NextResponse.json(
-      { message: 'Not found' },
-      { status: 404 }
-    )
+    return NextResponse.json({ message: "Project not found" }, { status: 404 });
   }
 
-  return NextResponse.json(project)
+  return NextResponse.json(project);
 }
